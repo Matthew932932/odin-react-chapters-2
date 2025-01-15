5608b4befec043cf1064e5d73fada6394fe27cf0
Matthew932932 <184472697+Matthew932932@users.noreply.github.com>
2025-01-15 11:36:14 +1100

    completed fetching data exercises

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,29 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WeirdImageComponent from './WeirdImageComponent'
+import Profile from './Profile';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <h2>Profile component</h2>
+      <div className="App">
+        <Profile delay={1000} />
+      </div>
+      <h2>--------------------</h2>
+      <h2>WeirdImageComponent</h2>
       <WeirdImageComponent />
       
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
